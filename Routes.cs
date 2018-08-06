d4e430a9e55fe5c2fe40a736094cb549e0b1a4bd
aaronamm <theeranitp@gmail.com>
2018-08-07 00:37:05 +0700

    initial setup plist xml response

---
+++ Routes.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+using CodeSanook.Common.Modules;
+
+namespace CodeSanook.AppRelease
+{
+    public class Routes : IRouteProvider
+    {
+        private static string moduleName = ModuleHelper.GetModuleName<Routes>();
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+            {
+                routes.Add(routeDescriptor);
+            }
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[]
+            {
+                new RouteDescriptor
+                {
+                    Name = "AppRelease",
+                    Priority = 100,
+                    Route = new Route(
+                        "AppRelease/{action}", //URL, cannot be start with / or ~
+                        new RouteValueDictionary {//default
+                            {"area", moduleName},
+                            {"controller", "Home"},
+                            {"action", "Index"}
+                        },
+                        new RouteValueDictionary(),//constraint
+                        new RouteValueDictionary {//data token
+                            {"area", moduleName}
+                        },
+                        new MvcRouteHandler())//handler
+                },
+            };
+        }
+    }
+}
